7768683e5d0fc08f8c89143916fe0a07a62c9430
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-04-28 16:55:41 +0530

    vaibhav-ops

--- vaibhav/recipes/ops.rb
+++ vaibhav/recipes/ops.rb
@@ -2,6 +2,10 @@ package "git" do
   action :install
 end
 
+execute "touch vaibhav" do
+  if { Dir.exist?("/home/ubuntu/git") }
+end
+
 git "/home/ubuntu/git" do
   repository "https://github.com/vaibhavttnd/gitclone.git"
   reference "master"
