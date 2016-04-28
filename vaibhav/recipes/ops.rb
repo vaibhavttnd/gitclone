5fca0fa6d33cea6342ed147d4f0d94ec1352d46b
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-04-28 18:00:45 +0530

    v4

--- vaibhav/recipes/ops.rb
+++ vaibhav/recipes/ops.rb
@@ -2,6 +2,9 @@ package "git" do
   action :install
 end
 
+
+#install git if required
+
 #execute "touch vaibhav.txt" do
 #  not_if { File.exist?("/vol/postgres/data") }
 #end
@@ -10,15 +13,23 @@ end
 #  not_if { File.exist?("/vol/postgres/data") }
 #end
 
-execute "touch /home/ubuntu/notexist.txt" do
-  not_if { Dir.exist?("/home/ubuntu/bit") }
+#execute "touch /home/ubuntu/notexist.txt" do
+#  not_if { Dir.exist?("/home/ubuntu/bit") }
+#end
+
+#clone if directory exits
+
+if Dir.exist?("/home/ubuntu/abc")
+	git "/home/ubuntu/git" do
+	  repository "https://github.com/vaibhavttnd/gitclone.git"
+   	  reference "master"
+  	  action :sync
+	end
+else
+	execute "touch /home/ubuntu/notexist2.txt" do
+        not_if { Dir.exist?("/home/ubuntu/bit") }
 end
 
 
-
-git "/home/ubuntu/git" do
-  repository "https://github.com/vaibhavttnd/gitclone.git"
-  reference "master"
-  action :sync
 end
 
