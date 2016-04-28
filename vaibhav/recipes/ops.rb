22df6a64218edf1cc8eba9df2b814681ba22792f
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-04-28 17:16:04 +0530

    v3

--- vaibhav/recipes/ops.rb
+++ vaibhav/recipes/ops.rb
@@ -2,9 +2,14 @@ package "git" do
   action :install
 end
 
-execute "touch vaibhav.txt" do
+#execute "touch vaibhav.txt" do
+#  not_if { File.exist?("/vol/postgres/data") }
+#end
+
+execute "touch /home/ubuntu/vaibhav2.txt" do
   not_if { File.exist?("/vol/postgres/data") }
 end
+
 
 git "/home/ubuntu/git" do
   repository "https://github.com/vaibhavttnd/gitclone.git"
