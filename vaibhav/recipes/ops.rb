a6aacd9df4a29acd85e166ea2cccce38e4783e69
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-04-29 10:52:40 +0530

    template

--- vaibhav/recipes/ops.rb
+++ vaibhav/recipes/ops.rb
@@ -1,6 +1,8 @@
 #include_recipe 'vaibhav::apache'
-include_recipe 'vaibhav::git'
-#install git if required
+#include_recipe 'vaibhav::git'
+
+
+include_recipe 'vaibhav::nginx'
 
 #execute "touch vaibhav.txt" do
 #  not_if { File.exist?("/vol/postgres/data") }
@@ -28,13 +30,24 @@ include_recipe 'vaibhav::git'
 #end
 #end
 
+# Final start
 
-if Dir.exist?("/home/ubuntu/git")
-         git "/home/ubuntu/git" do
-         repository "https://github.com/vaibhavttnd/gitclone.git"
-         reference "master"
-         action :sync
-       end
-else
-       execute "sudo mkdir /home/ubuntu/git" 
-end
+#if Dir.exist?("/home/ubuntu/git")
+#         git "/home/ubuntu/git" do
+#         repository "https://github.com/vaibhavttnd/gitclone.git"
+#         reference "master"
+#         action :sync
+#       end
+#else
+#       execute "sudo mkdir /home/ubuntu/git" 
+#end
+
+#final end
+
+template '/etc/nginx/sites-enabled/sample.com' do
+    source 'sample.com.erb'
+    owner 'root'
+    group 'root'
+    mode  '0755'
+    variables ( :port => '81')
+end	
