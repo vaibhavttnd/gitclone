926136e11adf954f989a6b0dbb3d19346ba26f18
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-04-29 15:31:53 +0530

    file delete

--- vaibhav/recipes/main.rb
+++ vaibhav/recipes/main.rb
@@ -30,7 +30,7 @@ include_recipe 'vaibhav::nginx'
 #end
 #end
 
-# Final start
+# Q1 start
 
 #if Dir.exist?("/home/ubuntu/git")
 #         git "/home/ubuntu/git" do
@@ -42,21 +42,30 @@ include_recipe 'vaibhav::nginx'
 #       execute "sudo mkdir /home/ubuntu/git" 
 #end
 
-#final end
-template '/etc/nginx/sites-enabled/vh1' do
-    source 'sample.com.erb'
-    owner 'root'
-    group 'root'
-    mode  '0755'
-    variables( :port => '80')
-end
+#Q1 end
 
-template '/etc/nginx/sites-enabled/vh2' do
-    source 'sample.com.erb'
-    owner 'root'
-    group 'root'
-    mode  '0755'
-    variables( :port => '81')
-end
+#Q2 start
+#template '/etc/nginx/sites-enabled/vh1' do
+#    source 'sample.com.erb'
+#    owner 'root'
+#    group 'root'
+#    mode  '0755'
+#    variables( :port => '80')
+#end
 
-execute "sudo service nginx restart"	
+#template '/etc/nginx/sites-enabled/vh2' do
+#    source 'sample.com.erb'
+#    owner 'root'
+#    group 'root'
+#    mode  '0755'
+#    variables( :port => '81')
+#end
+
+#execute "sudo service nginx restart"	
+
+#Q2 end
+
+
+file '/etc/nginx/sites-enabled/default' do	
+	action :delete
+end
